da9485a61f3f092818f2cd5b90e1cbffde7ef2bf
Peter Wells <petethebeatwells@gmail.com>
2020-10-19 21:41:47 -0700

    basic movement with animations added

--- scripts/Player.cs
+++ scripts/Player.cs
@@ -6,14 +6,15 @@ public class Player : KinematicBody2D
     // System.Diagnostics.Debug.WriteLine("this is a console log");
 
     // speed variables
-    public const float topSpeed = 100.0f;
-    public const float accel = 6.0f;
-    public const float deccel = 4.0f;
-    public const float currentAccel = 0.0f;
+    public const float topSpeed = 150.0f;
+    public const float accel = 30.0f;
+    public const float deccel = 40.0f;
+    public float currentAccel = 0.0f;
     // airtime variables
-    public const float jumpForce = 80.0f;
-    public const float gravity = 80.0f;
-    public const float maxFallSpeed = 1000.0f;
+    public const float jumpForce = 250.0f;
+    public const float gravity = 700.0f;
+    public const float maxFallSpeed = 4000.0f;
+    public const bool isOnFloor = true;
     // animation variables
     public bool facingRight = true;
     // attacking variables
@@ -29,6 +30,7 @@ public class Player : KinematicBody2D
 	{
         MoveInputCheck();
         ActionInputCheck();
+        UpdateAnimation();
 	}
 
 	
@@ -44,12 +46,76 @@ public class Player : KinematicBody2D
 
     void MoveInputCheck()
     {
+		// Jumping
+		if (Input.IsActionPressed("jump") && IsOnFloor()) {
+			velocity.y -= jumpForce;
+		}
 
+		// Moving left and right
+		if (Input.IsActionPressed("moveRight")) {
+			if (!facingRight) {
+				velocity.x = 0;
+				facingRight = true;
+			}
+			
+			currentAccel = accel; // eventually write function that adds curve instead of just assigning?
+
+			if (velocity.x < topSpeed) {
+				velocity.x += currentAccel;
+			}
+		} else if (Input.IsActionPressed("moveLeft")) {
+			if (facingRight) {
+				velocity.x = 0;
+				facingRight = false;
+			}
+			
+			currentAccel = accel; // eventually write function that adds curve instead of just assigning?
+			
+			if (velocity.x > -topSpeed) {
+				velocity.x -= currentAccel;
+			}
+		} else {
+			VelocityToZero(1f);
+		}
     }
+
+    public void VelocityToZero(float modifier)
+	{
+		currentAccel = deccel * modifier; // eventually write function that adds curve instead of just assigning?
+
+		if (velocity.x < 0) {
+			velocity.x += currentAccel;
+			if (velocity.x > 0) {
+				velocity.x = 0;
+			}
+		} else if (velocity.x > 0) {
+			velocity.x -= currentAccel;
+			if (velocity.x < 0) {
+				velocity.x = 0;
+			}
+		}
+	}
 
     void ActionInputCheck()
     {
 
+    }
+
+    void UpdateAnimation()
+    {
+        AnimatedSprite sprite = (AnimatedSprite)GetNode("AnimatedSprite");
+
+        if (velocity.x > 0) {
+            sprite.Play("runRight");
+        } else if (velocity.x < 0) {
+            sprite.Play("runLeft");
+        } else {
+            if (facingRight == true) {
+                sprite.Play("idleRight");
+            } else {
+                sprite.Play("idleLeft");
+            }
+        }
     }
 
 //  // Called every frame. 'delta' is the elapsed time since the previous frame.
